da2fd35f2cf30f64f91ec646609c213507345c74
Janaína Reis <lupieperola@gmail.com>
2024-10-11 11:21:18 -0300

    Incluído caso de teste para falha ao fazer login com credenciais inválidas.

--- cypress/e2e/lojinha.cy.js
+++ cypress/e2e/lojinha.cy.js
@@ -4,32 +4,46 @@ describe('Automatização de Login na Lojinha', () => {
   });
 
   it('Deve logar com sucesso usando credenciais válidas', () => {
-    cy.visit('http://165.227.93.41/lojinha-web/v2/');
-    
-    // Forçando a digitação no campo de usuário
     cy.get('input#usuario')
       .should('exist')
       .type(Cypress.env('CYPRESS_USERNAME'), { force: true });
-  
-    // Forçando a digitação no campo de senha
+
     cy.get('input#senha')
       .should('exist')
       .type(Cypress.env('CYPRESS_PASSWORD'), { force: true });
-  
+
     cy.get('button[type="submit"]').click();
-    
-    // Verificando a mensagem de boas vindas
+
+    // Verificando a mensagem de boas-vindas
     cy.contains('Boas vindas,').should('be.visible');
-    
-    // Espera pelo botão ou link de logout e clica nele (ajuste o seletor conforme necessário)
-    cy.get('a:contains("Sair")').should('be.visible').click(); // Usa um seletor mais genérico
+
+    // Espera pelo botão de logout e clica nele
+    cy.get('a:contains("Sair")').should('be.visible').click(); 
+  });
+
+  it('Não deve logar com credenciais inválidas', () => {
+    // Fornecendo um nome de usuário inválido
+    cy.get('input#usuario')
+      .should('exist')
+      .type('usuario_invalido', { force: true });
+
+    // Fornecendo uma senha inválida
+    cy.get('input#senha')
+      .should('exist')
+      .type('senha_invalida', { force: true });
+
+    cy.get('button[type="submit"]').click();
+
+    // Verificando a exibição da mensagem de erro
+    cy.contains('Falha ao fazer o login', { timeout: 10000 }).should('be.visible');
   });
 
   after(() => {
-    // Fecha o navegador no Chrome
+    // Fecha o navegador
     cy.window().then(win => win.close());
   });
 });
+
 
 
   
